72af6c449d326df18d2cdc5388bf7802b2f762fa
Ching Tse <67747846+tcm9439@users.noreply.github.com>
2023-01-16 20:16:24 +0800

    Update config so it can be import properly

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export * from "@/CacheFactory"
-export * from "@/config/CacheOption"
+import { Integer } from '@/util/CommonTypes';
+export { IKeyValueCache } from "@/cache"
+export { CacheFactory } from "@/CacheFactory"
+export { CacheOption, QueueConfig } from "@/config"
 export { CacheType, TimeoutMode } from "@/types"
